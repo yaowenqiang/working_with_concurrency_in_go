f20c018479754fbe4c988293cda9c7526ff4f12f
yaowenqiang <yaowenqiang111@163.com>
2026-05-11 23:36:08 +0800

    add waitgroup demo

--- main.go
+++ main.go
@@ -1,15 +1,41 @@
 package main
 
 import "fmt"
-import "time"
+import "sync"
+//import "time"
 
-func printSomething(s string) {
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	fmt.Println(s)
+
 }
 
 func main() {
-	go printSomething("This is the first ting to be printed!")
+	//go printSomething("This is the first ting to be printed!")
 
-	time.Sleep(1 * time.Second)
-	printSomething("This is the second thing to be printed!")
+	var wg sync.WaitGroup
+
+	words := []string{
+		"alpha",
+		"beta",
+		"gamma", 
+		"delta", 
+		"epsilon", 
+		"zeta", 
+		"eta", 
+		"theta", 
+		"iota",
+	}
+
+	//wg.Add(9)
+	wg.Add(len(words))
+
+	for i,x := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, x), &wg)
+	}
+
+	wg.Wait()
+	//time.Sleep(1 * time.Second)
+	wg.Add(1)
+	printSomething("This is the second thing to be printed!", &wg)
 }
