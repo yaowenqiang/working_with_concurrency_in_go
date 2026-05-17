00df6c6dda2efcd66e6c82fdf2e37ec3536cc93c
yaowenqiang <yaowenqiang111@163.com>
2026-05-17 14:33:40 +0800

    add buffered channel demo

---
+++ buffered_channels/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+import "time"
+
+func listenToChan(ch chan int) {
+	for {
+		i := <-ch
+		fmt.Println("Got ", i, " from channel!")
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	ch := make(chan int, 10)
+	go listenToChan(ch)
+
+	for i := 0; i <= 100; i++ {
+		fmt.Println("Sending ", i," to channel...")
+		ch<-i
+		fmt.Println("Send ", i," to channel...")
+	}
+
+	fmt.Println("Done")
+	close(ch)
+}
---
+++ sleep_barber/go.mod
@@ -0,0 +1,3 @@
+module sleep_barber
+
+go 1.23.0
