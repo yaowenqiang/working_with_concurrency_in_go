caef9bb6767df4cfbe0491d866b3d151f7a242a0
yaowenqiang <yaowenqiang111@163.com>
2026-05-18 23:58:35 +0800

    add final project

---
+++ final_project/cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+const webPort = "80"
+
+
+func main() {
+	// connect to the database
+	// create sessions
+	// crate channels
+	// create waitinggroup
+	// set up the application config
+	// set up mail
+	// listen for web connections
+
+}
